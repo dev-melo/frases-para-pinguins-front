a0c1b44bbea679f00df8ec347a02b4021334a9a2
Dev Melo <omelodev@gmail.com>
2026-06-20 16:15:04 -0300

    feat: css navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,22 @@
 import Link from "next/link";
 
-export default function Navbar(){
-    return(
-        
-        <nav>
-            <h1>Frases de Pinguins</h1>
-            <ul>
+export default function Navbar() {
+    return (
+
+        <nav className="flex justify-between items-center px-8 py-5 border-b bg-slate-200">
+            <h1 className="text-2xl font-bold tracking-tight">Frases de Pinguins</h1>
+            <ul className="flex gap-8">
                 <li>
-                    <Link href={"#"}>Home</Link>
+                    <Link href={"#"} className="text-gray-700 font-medium transition-colors hover:text-black">Home</Link>
                 </li>
                 <li>
-                    <Link href={"#"}>Frases</Link>
+                    <Link href={"#"} className="text-gray-700 font-medium transition-colors hover:text-black">Frases</Link>
                 </li>
                 <li>
-                    <Link href={"#"}>Pinguins</Link>
+                    <Link href={"#"} className="text-gray-700 font-medium transition-colors hover:text-black">Pinguins</Link>
                 </li>
             </ul>
-        </nav>             
-        
+        </nav>
+
     )
 }
